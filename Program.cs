a56d734d1549fa1b7828b6af61b0590bacfe49fb
Kosmas Suryopranoto <kosmas@formulatrix.com>
2021-06-16 19:55:19 +0700

    Even number OK

--- Program.cs
+++ Program.cs
@@ -10,8 +10,20 @@ namespace TrainingMultiplication
       return a + BasicMultiplication(a, b-1);
     }
 
+    static int AdvanceMultiplication(int a, int b)
+    {
+      if (a == 0 || b == 0) return 0; // when multiply by zero
+      else if (b == 1) return a; // when mutiply by one
+      else if ((b & 1) == 0) return AdvanceMultiplication(a + a, b >> 1); // when even number
+      else return 0;
+    }
+
     static void Main(string[] args)
     {
+      // Test something
+      // Console.WriteLine(-4 & 1);
+      // Console.WriteLine(-4 >> 1);
+
       // user input 
       Console.WriteLine("Enter first number:");
       string firstNumber = Console.ReadLine();
@@ -22,7 +34,14 @@ namespace TrainingMultiplication
       // checking
       Console.WriteLine("Your input: " + firstNumber + " and " + secondNumber);
       Console.WriteLine("The result is: ");
-      Console.WriteLine(BasicMultiplication(int.Parse(firstNumber), int.Parse(secondNumber)));
+
+      Console.WriteLine("Basic Result: ");
+      int basic_result = BasicMultiplication(int.Parse(firstNumber), int.Parse(secondNumber));
+      Console.WriteLine(basic_result);
+
+      Console.WriteLine("Advance Result: ");
+      int advanceResult = AdvanceMultiplication(Convert.ToInt32(firstNumber), Convert.ToInt32(secondNumber));
+      Console.WriteLine(advanceResult);
     }
   }
 }
