ac336e47640ab4896aae5b936c272b0e7cece09e
Kosmas Suryopranoto <kosmas@formulatrix.com>
2021-06-16 18:42:37 +0700

    Bismillah

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TrainingMultiplication
+{
+  class RunMultiplication
+  {
+    static int BasicMultiplication(int a, int b)
+    {
+      if (b == 0) return 0;
+      return a + BasicMultiplication(a, b-1);
+    }
+
+    static void Main(string[] args)
+    {
+      // user input 
+      Console.WriteLine("Enter first number:");
+      string firstNumber = Console.ReadLine();
+
+      Console.WriteLine("Enter second number:");
+      string secondNumber = Console.ReadLine();
+
+      // checking
+      Console.WriteLine("Your input: " + firstNumber + " and " + secondNumber);
+      Console.WriteLine("The result is: ");
+      Console.WriteLine(BasicMultiplication(int.Parse(firstNumber), int.Parse(secondNumber)));
+    }
+  }
+}
